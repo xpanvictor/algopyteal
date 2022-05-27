e2d0bc4a21d27567a1d77ddad229be4c7f1f9b89
xpanvictor <xpanvictor@gmail.com>
2022-05-27 14:22:00 +0100

    Started the on_bid code logic and edited the global constants

--- contract.py
+++ contract.py
@@ -1,14 +1,14 @@
 from pyteal import *
 
-
-#Create the constants
+# Create the constants
 seller_key = Bytes("seller_key")
 nft_id_key = Bytes("nft_id_key")
 start_time_key = Bytes("start_time_key")
 end_time_key = Bytes("end_time_key")
 reserve_amount_key = Bytes("reserve_amount_key")
 min_bid_increment_key = Bytes("min_bid_increment_key")
-lead_bid_account_key = Bytes("lead_bid_account_key")
+lead_bid_account_key = Bytes("lead_bid_amount_key")
+lead_bid_amount_key = Bytes("lead_bid_amount_key")
 
 # On create code logic
 on_create_start_time = Btoi(Txn.application_args[2])
@@ -46,6 +46,40 @@ on_setup = Seq(
     Approve(),
 )
 
+# On bid code logic
+on_bid_txn_index = Txn.group_index() - Int(1)
+on_bid_nft_holding = AssetHolding.balance(
+    Global.current_application_address(), App.globalGet(nft_id_key)
+)
+on_bid = Seq(
+    on_bid_nft_holding,
+    Assert(
+        And(
+            # we've set the auction
+            on_bid_nft_holding.hasValue(),
+            on_bid_nft_holding.value() > Int(0),
+            # the auction has started
+            Global.latest_timestamp() >= App.globalGet(start_time_key),
+            # the auction has not ended
+            Global.latest_timestamp() < App.globalGet(end_time_key),
+            # the actual bid payment is before the app call
+            Gtxn[on_bid_txn_index].type_enum() == TxnType.Payment,
+            Gtxn[on_bid_txn_index].sender() == Txn.sender(),
+            Gtxn[on_bid_txn_index].receiver() ==
+            Global.current_application_address(),
+            Gtxn[on_bid_txn_index].amount() >= Global.min_txn_fee(),
+        )
+    ),
+    If(
+        Gtxn[on_bid_txn_index].amount() >
+        App.globalGet(lead_bid_amount_key)
+    ).Then(
+        Seq(
+            If(App.globalGet(lead_bid_account_key))
+        )
+    )
+)
+
 # On call routing logic
 on_call_method = Txn.application_args[0]
 on_call = Cond(
@@ -70,4 +104,3 @@ program = Cond(
         Reject()
     ],
 )
-

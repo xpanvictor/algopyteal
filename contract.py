c6c89302096b87daafde2eb936882ff7a41eb22f
xpanvictor <xpanvictor@gmail.com>
2022-05-27 12:29:29 +0100

    Added the on_create sequence

--- contract.py
+++ contract.py
@@ -1,5 +1,26 @@
 from pyteal import *
 
+
+# On create code logic
+on_create_start_time = Btoi(Txn.application_args[2])
+on_create_end_time = Btoi(Txn.application_args[3])
+on_create = Seq(
+    App.globalPut(Bytes("seller_key"), Txn.application_args[0]),
+    App.globalPut(Bytes("nft_id_key"), Txn.application_args[1]),
+    App.globalPut(Bytes("start_time_key"), on_create_start_time),
+    App.globalPut(Bytes("end_time_key"), on_create_end_time),
+    App.globalPut(Bytes("reserve_amount_key"), Btoi(Txn.application_args[4])),
+    App.globalPut(Bytes("min_bid_increment_key"), Btoi(Txn.application_args[5])),
+    App.globalPut(Bytes("lead_bid_account_key"), Global.zero_address()),
+    Assert(
+        And(
+            Global.latest_timestamp() < on_create_start_time,
+            on_create_start_time < on_create_end_time
+        )
+    ),
+    Approve()
+)
+
 # Top level routing
 program = Cond(
     [Txn.application_id() == Int(0), on_create],

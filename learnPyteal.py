6d441e957790ed087467efe4c3a55773eb6cd4d8
xpanvictor <xpanvictor@gmail.com>
2022-05-27 09:16:56 +0100

    Learnt subroutines function with pyteal

--- learnPyteal.py
+++ learnPyteal.py
@@ -46,3 +46,12 @@ code = Seq(
     Approve()
 )
 
+
+# Subroutines (functions)
+@Subroutine(TealType.uint64)
+def isEven(i):
+    return i % Int(2) == Int(0)
+
+
+App.globalPut(Bytes("value_is_even"), isEven(Int(10)))
+
